0526c2e5e31da61899aa5cbde61d89e414110977
Alexander Ignatev <alexign473@gmail.com>
2022-06-10 11:22:20 +0300

    add articles

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,6 +1,23 @@
 import React from 'react';
-import './article.module.scss';
+import classNames from 'classnames';
+import styles from './article.module.scss';
 
-export const Article = () => {
-  return <div>Article</div>;
+export const Article = ({ articleFirst, imgUrl, date, title }) => {
+  const articleStyle = classNames(styles.article, {
+    [styles.articleFirst]: articleFirst,
+  });
+  return (
+    <div className={articleStyle}>
+      <div className={styles.image}>
+        <img src={imgUrl} alt='blog'></img>
+      </div>
+      <div className={styles.content}>
+        <div>
+          <span>{date}</span>
+          <h3>{title}</h3>
+        </div>
+        <span>Read Full Article</span>
+      </div>
+    </div>
+  );
 };
